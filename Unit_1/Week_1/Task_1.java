edef73d39910657383b1103d78eee24445667b22
vtu25124-art <vtu25124@veltech.edu.in>
2026-04-01 10:03:44 +0530

    Create Task_1.java

---
+++ Unit_1/Week_1/Task_1.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+class Task_1{
+
+    public int isEven(int input1) {
+
+        if (input2 % 2 == 0) {
+            return 2; // Returns 2 if the number is Even
+        } else {
+            return 1; // Returns 1 if the number is Odd
+        }
+    }
+}
