f2c5e60a86d9bb8d72c10853ffb451c4535a8d5c
vtu25124-art <vtu25124@veltech.edu.in>
2026-04-01 10:21:44 +0530

    Create Task_5.java

---
+++ Unit_1/Week_3/Task_5.java
@@ -0,0 +1,13 @@
+
+import java.io.*;
+import  java.util.*;
+import java.math.BigDecimal;
+class Task_5
+{
+public String addNumberStrings(String input1,String input2)
+{
+BigDecimal x = new BigDecimal(input1);
+        BigDecimal y = new BigDecimal(input2);
+        return String.valueOf(x.add(y));
+}
+}
