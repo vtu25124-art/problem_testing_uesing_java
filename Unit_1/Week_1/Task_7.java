9a37a0923904b81f1e81e9dfe71fbb222dec8599
vtu25124-art <vtu25124@veltech.edu.in>
2026-04-01 10:07:30 +0530

    Create Task_7.java

---
+++ Unit_1/Week_1/Task_7.java
@@ -0,0 +1,20 @@
+
+class Task_7{
+    public in EvenOddDigitsSum(int input1, String input2){
+        int sum = 0;
+        while (input1 > 0) {
+            int digit = input1 % 10;
+            if (input2.equalsIgnoreCase("even")) {
+                if (digit % 2 == 0) {
+                    sum += digit;
+                }
+            } else if (input2.equalsIgnoreCase("odd")) {
+                if (digit % 2 != 0) {
+                    sum += digit;
+                }
+            }
+            input1 /= 10;
+        }
+        return sum;
+   }
+}
