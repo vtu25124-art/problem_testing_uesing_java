30f5939684341a912980359f187d100e074a5d99
vtu25124-art <vtu25124@veltech.edu.in>
2026-04-01 10:04:31 +0530

    Create Task_2.java

---
+++ Unit_1/Week_1/Task_2.java
@@ -0,0 +1,12 @@
+public class Task_2{
+    public static void main(String[] args) {
+        // 1. Create an array of integers
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        // 2. Define the index you want to access
+        int index = 2; 
+
+        // 3. Access and print the element
+        System.out.println("Element at index " + index + " is: " + numbers[index]);
+    }
+}
